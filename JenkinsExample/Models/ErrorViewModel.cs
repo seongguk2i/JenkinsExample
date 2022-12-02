8afba324c1c4b4441ab38483aa1b9058dfd6afbe
김성국 <live8681@naver.com>
2022-12-02 14:59:44 +0900

    project copy

---
+++ JenkinsExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JenkinsExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
